0bf39b9104431c2341c9495d35e49556dbbffa45
Ilaria <ilaria.callegari@gmail.com>
2021-09-01 20:02:40 +0100

    Update ImageGeology.js

--- src/components/ImageGeology.js
+++ src/components/ImageGeology.js
@@ -1,11 +1,3 @@
-// import ImgMercuryGeology from "../assets/geology-mercury.png";
-// import ImgVenusGeology from "../assets/geology-venus.png";
-// import ImgEarthGeology from "../assets/geology-earth.png";
-// import ImgMarsGeology from "../assets/geology-mars.png";
-// import ImgJupiterGeology from "../assets/geology-jupiter.png";
-// import ImgSaturnGeology from "../assets/geology-saturn.png";
-// import ImgUranusGeology from "../assets/geology-uranus.png";
-// import ImgNeptuneGeology from "../assets/geology-neptune.png";
 import useStyles from "../styles/main-style";
 import classNames from "classnames";
 
